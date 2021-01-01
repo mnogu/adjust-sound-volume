75beac5ccbc0fb06dd9842061503264ca6a1ee5d
Muneyuki Noguchi <nogu.dev@gmail.com>
2021-01-02 00:23:12 +0900

    Change the notice

--- __init__.py
+++ __init__.py
@@ -1,7 +1,18 @@
-# -*- coding: utf-8 -*-
-# Copyright: Muneyuki Noguchi
-# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-
+# Anki 2.1.x add-on to adjust the sound volume
+# Copyright (C) 2021  Muneyuki Noguchi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 Adjust the sound volume in 2.1.x.
 """
